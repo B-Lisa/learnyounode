2883ae888a1775433b227e63022e5b5e19badaff
B-Lisa <lisabui@hotmail.de>
2020-03-16 16:46:22 +0000

    readFile

--- my-first-io.js
+++ my-first-io.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var buf = fs.readFileSync(process.argv[2]);
+var buf = fs.readFileSync('/workspace/learnyounode/my-first-io.js')(process.argv[2]);
 var str = buf.toString();
 var strArr = str.split('\n');
 console.log(strArr.length);
