ee8bf78ef191320038de0ecd633548ba5a6f781f
B-Lisa <lisabui@hotmail.de>
2020-03-16 16:50:30 +0000

    solution

--- my-first-io.js
+++ my-first-io.js
@@ -1,6 +1,1 @@
-var fs = require('fs');
-var file = fs.readFileSync('/workspace/learnyounode/my-first-io.js');
-var buf = file(process.argv[2]);
-var str = buf.toString();
-var strArr = str.split('\n');
-console.log(strArr.length);
+console.log(require('fs').readFileSync(process.argv[2], {encoding: 'utf8'}).split('\n').length - 1);
